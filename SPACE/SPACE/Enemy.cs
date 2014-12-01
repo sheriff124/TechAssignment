ec2ad77d2fc750864e55cfb1c2ab8c0683909181
sheriff124 <tomnottingham124@hotmail.com>
2014-12-01 20:05:48 +0000

    Started Enemy Class

---
+++ SPACE/SPACE/Enemy.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+using Sce.PlayStation.Core;
+using Sce.PlayStation.Core.Environment;
+using Sce.PlayStation.Core.Graphics;
+using Sce.PlayStation.Core.Input;
+using Sce.PlayStation.Core.Audio;
+
+using Sce.PlayStation.HighLevel.GameEngine2D;
+using Sce.PlayStation.HighLevel.GameEngine2D.Base;
+
+
+
+namespace SPACE
+{
+	public class Enemy : Entity
+	{
+		private enum DirState { Still, Left, Right };
+		private DirState dirState;
+		private float moveSpeed;
+		public Enemy ()
+		{
+			texInfo = new TextureInfo ("/SPACE/textures/bird.png");
+			sprite = new SpriteUV (texInfo);
+			sprite.Quad.S = texInfo.TextureSizef;
+			sprite.Position = new Vector2 (50, 50);
+			dirState = DirState.Still;
+			moveSpeed = 5.0f;
+		}	
+		override public void Update(float _deltaTime)
+		{
+			switch(dirState)
+			{
+				case DirState.Right:
+					sprite.Position = new Vector2(sprite.Position.X+moveSpeed, sprite.Position.Y);
+				break;
+				
+				case DirState.Left:
+					sprite.Position = new Vector2(sprite.Position.X-moveSpeed, sprite.Position.Y);
+				break;
+			}
+		}
+	}
+}
+
